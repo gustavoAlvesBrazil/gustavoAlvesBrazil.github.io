00db2af6d14ac548bbd57ad7128fcc999a64ee98
Gustavo Alves da Silva <gugagranprime2016@gmail.com>
2022-01-14 21:31:14 -0300

    alterei o js

--- js/slider.js
+++ js/slider.js
@@ -1,13 +1,8 @@
 			var slideAtual = 0;
-			var timeToChangeInMilis = 3000;
+			var timeToChangeInMilis = 5000;
 			var mockup = document.getElementById('mockup');
 			
 			var slides = ['images/mockup.png', 'images/mockup1.png', 'images/mockup2.png', 'images/mockup3.png', 'images/mockup4.png'];
-			
-			//console.log('trocando para: ' + slides[slideAtual]);
-			//mockup.src = slides[slideAtual];
-			
-			//slideAtual = 1;
 			
 			trocaSlides();
 			
@@ -19,7 +14,6 @@
 			function trocaSlides() {
 				
 				if (slideAtual < slides.length) {
-					console.log('trocando para: ' + slides[slideAtual]);
 					mockup.src = slides[slideAtual];
 					slideAtual++;
 				} else {
